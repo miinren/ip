e8ebdebc300503c23c056d725458fe49adb8ab24
miinren <seahminren2@gmail.com>
2025-08-27 02:11:08 +0800

    Level 8. Date and Time

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,41 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    private String by;
+    private LocalDateTime byDT;
+    private LocalDate byD;
+
+    private static final DateTimeFormatter inputDT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final DateTimeFormatter outputDT = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+    private static final DateTimeFormatter inputD = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter outputD = DateTimeFormatter.ofPattern("MMM dd yyyy");
 
     public Deadline(String description, String by) {
         super(description, TaskType.DEADLINE);
-        this.by = by;
+
+        try {
+            byDT = LocalDateTime.parse(by, inputDT);
+        } catch (DateTimeParseException e) {
+            try {
+                byDT = null;
+                byD = LocalDate.parse(by, inputD);
+            } catch (DateTimeParseException e2) {
+                throw new IllegalArgumentException("Invalid date/time format. Use yyyy-MM-dd or yyyy-MM-dd HHmm");
+            }
+        }
     }
 
     @Override
     public String toString() {
-        return "[D] " + super.toString() + " (by: " + this.by + ")";
+        String byStr = byDT == null ? byD.format(outputD) : byDT.format(outputDT);
+        return "[D] " + super.toString() + " (by: " + byStr + ")";
     }
 
     @Override
     public String makePretty() {
-        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
+        String byStr = byDT == null ? byD.format(outputD) : byDT.format(outputDT);
+        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + byStr;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,20 +1,54 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
-    private String from;
-    private String to;
+    private LocalDateTime fromDT;
+    private LocalDateTime toDT;
+    private LocalDate fromD;
+    private LocalDate toD;
+
+    private static final DateTimeFormatter inputDT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final DateTimeFormatter outputDT = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+    private static final DateTimeFormatter inputD = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter outputD = DateTimeFormatter.ofPattern("MMM dd yyyy");
 
     public Event(String description, String from, String to) {
         super(description, TaskType.EVENT);
-        this.from = from;
-        this.to = to;
+
+        try {
+            fromDT = LocalDateTime.parse(from, inputDT);
+        } catch (DateTimeParseException e) {
+            try {
+                fromDT = null;
+                fromD = LocalDate.parse(from, inputD);
+            } catch (DateTimeParseException e2) {
+                throw new IllegalArgumentException("Invalid date/time format. Use yyyy-MM-dd or yyyy-MM-dd HHmm");
+            }
+        }
+
+        try {
+            toDT = LocalDateTime.parse(to, inputDT);
+        } catch (DateTimeParseException e) {
+            toDT = null;
+            toD = LocalDate.parse(to, inputD);
+        }
     }
 
     @Override
     public String toString() {
-        return "[E] " + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+        String fromStr = fromDT == null ? fromD.format(outputD) : fromDT.format(outputDT);
+        String toStr = toDT == null ? toD.format(outputD) : toDT.format(outputDT);
+
+        return "[E] " + super.toString() + " (from: " + fromStr + " to: " + toStr + ")";
     }
 
     @Override
     public String makePretty() {
-        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + from + " | " + to;
+        String fromStr = fromDT == null ? fromD.format(outputD) : fromDT.format(outputDT);
+        String toStr = toDT == null ? toD.format(outputD) : toDT.format(outputDT);
+
+        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + fromStr + " | " + toStr;
     }
 }
