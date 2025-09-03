b808879c25aba69a1c764b618fa9396bbe1ccb90
miinren <seahminren2@gmail.com>
2025-09-03 14:13:36 +0800

    Add javadoc for WelcomeCommand

--- src/main/java/peanutbutter/commands/WelcomeCommand.java
+++ src/main/java/peanutbutter/commands/WelcomeCommand.java
@@ -4,7 +4,18 @@ import peanutbutter.exceptions.JuinException;
 import peanutbutter.tasks.TaskList;
 import peanutbutter.ui.Ui;
 
+/**
+ * A command that displays the welcome message to the user.
+ */
 public class WelcomeCommand extends Command {
+    /**
+     * Executes the WelcomeCommand by showing the welcome message via the UI.
+     *
+     * @param taskList The current TaskList
+     * @param ui The UI object used to display messages
+     * @return false since this command does not exit the application
+     * @throws JuinException if any error occurs while interacting with the UI
+     */
     @Override
     public boolean run(TaskList taskList, Ui ui) throws JuinException {
         ui.welcomeMessage();
